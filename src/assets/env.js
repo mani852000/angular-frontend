7740739127567099d895b59b22cef14f5d271cbf
mani852000 <114754577+mani852000@users.noreply.github.com>
2022-11-01 13:53:58 +0530

    Update env.js

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,6 +2,6 @@
     window["env"] = window["env"] || {};
   
     // Environment variables
-    window["env"]["apiUrl"] = "http://3.111.187.158:8090";
+    window["env"]["apiUrl"] = "http://10.10.100.177:8080/springboot-prometheus-demo/";
     window["env"]["debug"] = true;
   })(this);
